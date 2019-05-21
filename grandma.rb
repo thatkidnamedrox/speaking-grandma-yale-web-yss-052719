54eb737e4a9fed68d676360592680fad00796f56
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-21 15:40:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   if (phrase != phrase.upcase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif (phrase == "I LOVE YOU GRANDMA!")
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
 
