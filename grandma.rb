d2ae1b105080332fbe2887c7db9e322beac879e5
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-21 15:36:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,11 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if (phrase != phrase.upcase)
+    return "HUH?! SPEAK UP, SONNY!"
+  if (phrase == "I LOVE YOU GRANDMA")
+    return "I LOVE YOU TOO PUMPKIN"
+  return "NO, NOT SINCE 1938!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
